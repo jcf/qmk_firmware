10643fa67d37602390f9dbf70f1869b576144a04
James Conroy-Finn <git@invetica.co.uk>
2018-02-25 17:07:41 +0000

    Align keymap

--- keyboards/ergodox_ez/keymaps/jcf/keymap.c
+++ keyboards/ergodox_ez/keymaps/jcf/keymap.c
@@ -40,23 +40,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Otherwise, it needs KC_*
   [BASE] = LAYOUT_ergodox(  // layer 0 : default
                           // left hand
-                          KC_EQL,         KC_1,         KC_2,   KC_3,        KC_4,        KC_5,   KC_LEFT,
-                          KC_TAB,         KC_Q,         KC_D,   KC_R,        KC_W,        KC_B,   TG(SYMB),
-                          CTL_T(KC_ESC),  KC_A,         KC_S,   SFT_T(KC_H), CTL_T(KC_T), KC_G,
-                          KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_M,        KC_C,        KC_V,   ALL_T(KC_NO),
-                          LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),       KC_LALT,     KC_LGUI,
-                          ALT_T(KC_APP),  KC_LGUI,
+                          KC_EQL,        KC_1,        KC_2,     KC_3,          KC_4,        KC_5,    KC_LEFT,
+                          KC_TAB,        KC_Q,        KC_D,     KC_R,          KC_W,        KC_B,    TG(SYMB),
+                          CTL_T(KC_ESC), KC_A,        KC_S,     SFT_T(KC_H),   CTL_T(KC_T), KC_G,
+                          KC_LSFT,       CTL_T(KC_Z), KC_X,     KC_M,          KC_C,        KC_V,    ALL_T(KC_NO),
+                          LT(SYMB,       KC_GRV),     KC_QUOT,  LALT(KC_LSFT), KC_LALT,     KC_LGUI,
+                          ALT_T(KC_APP), KC_LGUI,
                           KC_LEFT,
-                          KC_SPC,KC_BSPC,KC_RIGHT,
+                          KC_SPC,        KC_BSPC,     KC_RIGHT,
                           // right hand
-                          KC_RGHT,     KC_6,   KC_7,         KC_8,         KC_9,   KC_0,             KC_MINS,
-                          TG(MDIA),    KC_J,   KC_F,         KC_U,         KC_P,   KC_SCLN,          KC_BSLS,
-                          KC_Y,   RCTL_T(KC_N), RSFT_T(KC_E), KC_O,   LT(MDIA, KC_I),   GUI_T(KC_QUOT),
-                          MEH_T(KC_NO),KC_K,   KC_L,         KC_COMM,      KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                          KC_RGUI,      KC_RALT,      KC_LBRC,KC_RBRC,          KC_FN1,
-                          KC_LALT,        CTL_T(KC_ESC),
+                          KC_RGHT,      KC_6,          KC_7,         KC_8,    KC_9,    KC_0,           KC_MINS,
+                          TG(MDIA),     KC_J,          KC_F,         KC_U,    KC_P,    KC_SCLN,        KC_BSLS,
+                          KC_Y,         RCTL_T(KC_N),  RSFT_T(KC_E), KC_O,    LT(MDIA, KC_I),          GUI_T(KC_QUOT),
+                          MEH_T(KC_NO), KC_K,          KC_L,         KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
+                          KC_RGUI,      KC_RALT,       KC_LBRC,      KC_RBRC, KC_FN1,
+                          KC_LALT,      CTL_T(KC_ESC),
                           KC_UP,
-                          KC_DOWN,KC_TAB, KC_ENT
+                          KC_DOWN,      KC_TAB,        KC_ENT
                             ),
   /* Keymap 1: Symbol Layer
    *
