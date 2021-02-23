bbfd5874a48d9d978b28c48b5699335392689374
James Conroy-Finn <git@invetica.co.uk>
2021-02-23 09:27:56 +0000

    Add < and > to symbol layer

--- keyboards/ergodox_ez/keymaps/jcf/keymap.c
+++ keyboards/ergodox_ez/keymaps/jcf/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
    * |        |   #  |   $  |   (  |   )  |   `  |------|           |------|PgDown|   4  |   5  |   6  |   +  |        |
    * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-   * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+   * |        |   %  |   ^  |   <  |   >  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
    * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
    *   | EPRM |      |      |      |      |                                       |      |    . |   0  |   =  |      |
    *   `----------------------------------'                                       `----------------------------------'
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
                           KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
                           KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-                          KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+                          KC_TRNS,KC_PERC,KC_CIRC,KC_LT,KC_GT,KC_TILD,KC_TRNS,
                           EPRM,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                           RGB_MOD,KC_TRNS,
                           KC_TRNS,
